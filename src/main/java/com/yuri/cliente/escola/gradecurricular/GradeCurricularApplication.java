04615d1f6a0b4a6cd1e13fb2b1a525ba8e3d1f3f
yurissoares <yurissoares@outlook.com>
2020-08-21 21:39:50 -0300

    Criação do projeto

---
+++ src/main/java/com/yuri/cliente/escola/gradecurricular/GradeCurricularApplication.java
@@ -0,0 +1,13 @@
+package com.yuri.cliente.escola.gradecurricular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradeCurricularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradeCurricularApplication.class, args);
+	}
+
+}
